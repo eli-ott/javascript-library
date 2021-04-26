7e8fc964ae5d0ad8b9e7d60e8af60e918a224724
eliott <eliottbidault@gmail.com>
2021-04-26 17:11:40 +0200

    made the form css

--- src/class/form.js
+++ src/class/form.js
@@ -1,6 +1,7 @@
 let checkClassInterval = setInterval(() => {
     if (document.querySelectorAll('.signinForm').length
-        || document.querySelectorAll('.signupForm').length) {
+        || document.querySelectorAll('.signupForm').length
+        || document.querySelectorAll('.emailForm').length) {
         clearInterval(checkClassInterval);
         createForm();
     }
@@ -18,6 +19,12 @@ let form = {
         <input required type="password" placeholder="Password"></input>
         <input required type="password" placeholder="Pasword confirmation"></input>
         <input required type="submit" value="SIGN UP"></input>
+    `,
+    email: `
+        <input required type="email" placeholder="Email"></input>
+        <input required type="text" placeholder="Subject"></input>
+        <input required type="text" placeholder="Content"></input>
+        <input required type="submit" value="SEND MAIL"></input>
     `
 };
 
@@ -25,11 +32,19 @@ const createForm = () => {
     if (document.querySelectorAll('.signinForm').length > 0) {
         for (let i = 0; i < document.querySelectorAll('.signinForm').length; i++) {
             document.getElementsByClassName('signinForm')[i].innerHTML = form.signin;
+            document.getElementsByClassName('signinForm')[i].style.height = '25vh';
         }
     }
-    if (document.querySelectorAll('.signupForm').length) {
+    if (document.querySelectorAll('.signupForm').length > 0) {
         for (let i = 0; i < document.querySelectorAll('.signupForm').length; i++) {
             document.getElementsByClassName('signupForm')[i].innerHTML = form.signup;
+            document.getElementsByClassName('signinForm')[i].style.height = '35vh';
+        }
+    }
+    if (document.querySelectorAll('.emailForm').length > 0) {
+        for (let i = 0; i < document.querySelectorAll('.emailForm').length; i++) {
+            document.getElementsByClassName('emailForm')[i].innerHTML = form.email;
+            document.getElementsByClassName('signinForm')[i].style.height = '30vh';
         }
     }
 }
